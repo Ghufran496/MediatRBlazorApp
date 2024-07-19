4a535199b6a2a3f5205bbb16cdba7f5774d40f80
Ghufran Ullah <ghufran.chaudary12@gmail.com>
2024-07-19 20:49:08 +0500

    Code changes for practice

--- BlazorUI/obj/Debug/net7.0/BlazorUI.AssemblyInfo.cs
+++ BlazorUI/obj/Debug/net7.0/BlazorUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2121d93e859b5347a7af331430defb79e9d19e90")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- demoLibrary/obj/Debug/net7.0/demoLibrary.AssemblyInfo.cs
+++ demoLibrary/obj/Debug/net7.0/demoLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("demoLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2121d93e859b5347a7af331430defb79e9d19e90")]
 [assembly: System.Reflection.AssemblyProductAttribute("demoLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("demoLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
